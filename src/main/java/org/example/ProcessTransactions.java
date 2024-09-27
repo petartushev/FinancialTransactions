674b4b23857eef698f2882c11108913b6ede01e7
Petar Tushev <petartushev@hotmail.com>
2024-09-27 22:07:37 +0200

    Preliminary iceberg support.

--- src/main/java/org/example/ProcessTransactions.java
+++ src/main/java/org/example/ProcessTransactions.java
@@ -8,11 +8,20 @@ import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsIni
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.example.process_functions.WatermarkPrintProcessFunction;
-import org.example.sinks.ExactlyOncePostgresSink;
-import org.example.sinks.PostgresSink;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.types.Row;
+import org.apache.iceberg.aws.s3.S3FileIO;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.flink.CatalogLoader;
+import org.apache.iceberg.flink.TableLoader;
+import org.apache.iceberg.flink.sink.FlinkSink;
+import org.apache.hadoop.conf.Configuration;
 import pojo.Transaction;
 import timestamp_utils.TransactionWatermarkStrategy;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ProcessTransactions {
@@ -42,9 +51,48 @@ public class ProcessTransactions {
                         new Gson().fromJson(value, Transaction.class))
                 .assignTimestampsAndWatermarks(new TransactionWatermarkStrategy());
 
-//        transactionDataStream.print();
+        TableSchema tableSchema = TableSchema.builder()
+                .field("transactionId", DataTypes.STRING())
+                .field("sendingClientAccountNumber", DataTypes.STRING())
+                .field("receivingClientAccountNumber", DataTypes.STRING())
+                .field("amount", DataTypes.DOUBLE())
+                .build();
 
-        transactionDataStream.addSink(new ExactlyOncePostgresSink(jdbcUrl, username, password));
+        Map<String, String> catalogProperties = new HashMap<>();
+        catalogProperties.put("uri", "http://localhost:8181");  // REST Catalog URI
+        catalogProperties.put("warehouse", "s3://warehouse/");  // S3 warehouse location
+        catalogProperties.put("io-impl", S3FileIO.class.getName());  // Use S3 File IO
+        catalogProperties.put("s3.endpoint", "http://minio:9000");  // MinIO endpoint
+        catalogProperties.put("s3.access-key", "admin");  // MinIO access key
+        catalogProperties.put("s3.secret-key", "password");  // MinIO secret key
+        catalogProperties.put("s3.path-style-access", "true");  // Required for MinIO
+
+        Configuration hadoopConf = new Configuration();
+        hadoopConf.set("fs.s3a.access.key", "admin");
+        hadoopConf.set("fs.s3a.secret.key", "password");
+        hadoopConf.set("fs.s3a.endpoint", "http://minio:9000");
+        hadoopConf.set("fs.s3a.path.style.access", "true");
+
+        CatalogLoader catalogLoader = CatalogLoader.custom(
+                "my_catalog",  // Name of the catalog
+                catalogProperties,  // Catalog properties (e.g., S3 config)
+                hadoopConf,  // Hadoop configuration
+                "org.apache.iceberg.rest.RESTCatalog"  // Catalog implementation class (REST)
+        );
+
+        TableLoader tableLoader = TableLoader.fromCatalog(catalogLoader, TableIdentifier.of("my_db", "transactions"));
+
+        DataStream<Row> rowDataStream = transactionDataStream.map(
+                transaction -> Row.of(transaction.getTransactionId(),
+                        transaction.getSendingClientAccountNumber(),
+                        transaction.getReceivingClientAccountNumber(),
+                        transaction.getAmount())
+        );
+
+        FlinkSink.forRow(rowDataStream, tableSchema)
+                .tableLoader(tableLoader)
+                .overwrite(false)
+                .append();
 
         try{
             env.execute(ProcessTransactions.class.getName());
