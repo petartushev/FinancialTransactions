117c45d2eab1d6a636c824c5e475e601629e8fe3
Petar Tushev <petartushev@hotmail.com>
2024-11-23 10:12:01 +0100

    Removed unnecessary variables.

--- src/main/java/org/example/Variables.java
+++ src/main/java/org/example/Variables.java
@@ -6,7 +6,7 @@ public class Variables {
     final static String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
 
     final static String KAFKA_BOOSTRAP_SERVERS = "localhost:9092";
-    final static String KAFKA_TOPIC = "transactions";
+    final static String KAFKA_TOPIC = "transaction";
     final static String KAFKA_GROUP_ID = "transactions_group";
 
     final static String CATALOG_NAME = "bank_transactions_catalog";
@@ -16,6 +16,4 @@ public class Variables {
     final static String REST_CATALOG_URI = "http://localhost:8181";
     final static String S3_WAREHOUSE_LOCATION = "s3://warehouse/";
     final static String MINIO_API_ENDPOINT = "http://localhost:9000";
-    final static String S3_ACCESS_KEY = "admin";
-    final static String S3_SECRET_KEY = "password";
 }
