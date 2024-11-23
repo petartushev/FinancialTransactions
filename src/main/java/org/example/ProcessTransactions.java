94e150cfe96c2402f5f387b2b19f36420c7c186e
Petar Tushev <petartushev@hotmail.com>
2024-11-23 10:06:02 +0100

    Iceberg sink for transactions added.

--- src/main/java/org/example/ProcessTransactions.java
+++ src/main/java/org/example/ProcessTransactions.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.google.gson.Gson;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
@@ -9,7 +10,9 @@ import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 import org.apache.iceberg.aws.s3.S3FileIO;
 import org.apache.iceberg.catalog.TableIdentifier;
@@ -27,23 +30,32 @@ import java.util.Map;
 public class ProcessTransactions {
     public static void main(String[] args) throws Exception {
 
-        final String jdbcUrl = "jdbc:postgresql://localhost:5432/postgres";
-        final String username = "postgres";
-        final String password = "postgres";
+        Dotenv dotenv = Dotenv.load();
+
+        String AWS_ACCESS_KEY_ID = dotenv.get("AWS_ACCESS_KEY_ID");
+        String AWS_SECRET_ACCESS_KEY = dotenv.get("AWS_SECRET_ACCESS_KEY");
 
         KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
-                .setBootstrapServers("localhost:9092")
-                .setTopics("transaction")
-                .setGroupId("transactions_group")
+                .setBootstrapServers(Variables.KAFKA_BOOSTRAP_SERVERS)
+                .setTopics(Variables.KAFKA_TOPIC)
+                .setGroupId(Variables.KAFKA_GROUP_ID)
                 .setStartingOffsets(OffsetsInitializer.latest())
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        env.getConfig().setAutoWatermarkInterval(2000L);
-        env.enableCheckpointing(30000);  // e.g., 60000 for every minute
+        env.getConfig().setAutoWatermarkInterval(100L);
+        env.enableCheckpointing(2000);  // e.g., 60000 for every minute
         env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+
+        EnvironmentSettings settings = EnvironmentSettings.newInstance()
+                .inStreamingMode()
+                .withBuiltInCatalogName(Variables.CATALOG_NAME)
+                .withBuiltInDatabaseName(Variables.ICEBERG_DB_NAME)
+                .build();
+
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(env, settings);
 
         DataStream<String> dataStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka source");
 
@@ -51,43 +63,50 @@ public class ProcessTransactions {
                         new Gson().fromJson(value, Transaction.class))
                 .assignTimestampsAndWatermarks(new TransactionWatermarkStrategy());
 
+
         TableSchema tableSchema = TableSchema.builder()
-                .field("transactionId", DataTypes.STRING())
-                .field("sendingClientAccountNumber", DataTypes.STRING())
-                .field("receivingClientAccountNumber", DataTypes.STRING())
-                .field("amount", DataTypes.DOUBLE())
+                .field("transactionId", DataTypes.STRING().notNull())
+                .field("sendingClientAccountNumber", DataTypes.STRING().notNull())
+                .field("receivingClientAccountNumber", DataTypes.STRING().notNull())
+                .field("amount", DataTypes.DOUBLE().notNull())
                 .build();
 
         Map<String, String> catalogProperties = new HashMap<>();
-        catalogProperties.put("uri", "http://localhost:8181");  // REST Catalog URI
-        catalogProperties.put("warehouse", "s3://warehouse/");  // S3 warehouse location
+        catalogProperties.put("uri", Variables.REST_CATALOG_URI);  // REST Catalog URI
+        catalogProperties.put("warehouse", Variables.S3_WAREHOUSE_LOCATION);  // S3 warehouse location
         catalogProperties.put("io-impl", S3FileIO.class.getName());  // Use S3 File IO
-        catalogProperties.put("s3.endpoint", "http://minio:9000");  // MinIO endpoint
-        catalogProperties.put("s3.access-key", "admin");  // MinIO access key
-        catalogProperties.put("s3.secret-key", "password");  // MinIO secret key
+        catalogProperties.put("s3.endpoint", Variables.MINIO_API_ENDPOINT);  // MinIO endpoint http://minio:9000
+        catalogProperties.put("s3.access-key", AWS_ACCESS_KEY_ID);  // MinIO access key
+        catalogProperties.put("s3.secret-key", AWS_SECRET_ACCESS_KEY);  // MinIO secret key
         catalogProperties.put("s3.path-style-access", "true");  // Required for MinIO
 
         Configuration hadoopConf = new Configuration();
-        hadoopConf.set("fs.s3a.access.key", "admin");
-        hadoopConf.set("fs.s3a.secret.key", "password");
-        hadoopConf.set("fs.s3a.endpoint", "http://minio:9000");
+        hadoopConf.set("fs.s3a.access.key", AWS_ACCESS_KEY_ID);
+        hadoopConf.set("fs.s3a.secret.key", AWS_SECRET_ACCESS_KEY);
+        hadoopConf.set("fs.s3a.endpoint", Variables.MINIO_API_ENDPOINT);
         hadoopConf.set("fs.s3a.path.style.access", "true");
 
         CatalogLoader catalogLoader = CatalogLoader.custom(
-                "my_catalog",  // Name of the catalog
+                Variables.CATALOG_NAME,  // Name of the catalog
                 catalogProperties,  // Catalog properties (e.g., S3 config)
                 hadoopConf,  // Hadoop configuration
                 "org.apache.iceberg.rest.RESTCatalog"  // Catalog implementation class (REST)
         );
 
-        TableLoader tableLoader = TableLoader.fromCatalog(catalogLoader, TableIdentifier.of("my_db", "transactions"));
+
+        TableLoader tableLoader = TableLoader.fromCatalog(catalogLoader, TableIdentifier.of(
+                Variables.ICEBERG_DB_NAME, Variables.ICEBERG_DB_TABLE_NAME
+                )
+        );
 
         DataStream<Row> rowDataStream = transactionDataStream.map(
-                transaction -> Row.of(transaction.getTransactionId(),
+                transaction -> Row.of(
+                        transaction.getTransactionId(),
                         transaction.getSendingClientAccountNumber(),
                         transaction.getReceivingClientAccountNumber(),
                         transaction.getAmount())
         );
+
 
         FlinkSink.forRow(rowDataStream, tableSchema)
                 .tableLoader(tableLoader)
@@ -100,6 +119,7 @@ public class ProcessTransactions {
         catch (Exception e){
             e.printStackTrace();
         }
+
 
     }
 }
